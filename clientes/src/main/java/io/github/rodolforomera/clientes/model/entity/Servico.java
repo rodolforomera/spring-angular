4b6c7e2f476cacf457d1f40cdff29207e34546b1
rodolforomera <rodolforomeras@gmail.com>
2020-12-09 08:40:18 -0300

    Adicionados models e lombok

---
+++ clientes/src/main/java/io/github/rodolforomera/clientes/model/entity/Servico.java
@@ -0,0 +1,27 @@
+package io.github.rodolforomera.clientes.model.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column
+    private BigDecimal valor;
+
+}
